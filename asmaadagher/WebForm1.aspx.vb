3771801827f45cacf0eb403ebf2bb0f21b3cdb8c
asmaadagher <asmaadagher@gmail.com>
2013-05-09 22:46:43 +0300

    asmaadagher

---
+++ asmaadagher/WebForm1.aspx.vb
@@ -0,0 +1,11 @@
+﻿Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub CheckBox1_CheckedChanged(ByVal sender As Object, ByVal e As EventArgs) Handles drink.CheckedChanged
+
+    End Sub
+End Class
